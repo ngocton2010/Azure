475957109959beefc043ff20f589a85af035503c
NgocTon2010 <supevicto@gmail.com>
2021-09-26 16:56:04 +0700

    first commit

---
+++ gk/svr_model.py
@@ -0,0 +1,14 @@
+
+from flask import Flask, render_template
+
+# Khởi tạo Flask
+app = Flask(__name__)
+
+# Hàm xử lý request
+@app.route("/", methods=['GET'])
+def home_page():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='192.168.1.93', debug=False)
